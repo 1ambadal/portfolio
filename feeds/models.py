34d764184de49960a7401df2421b0baca4c028cc
Badal Panwar <badalpanwar@tradeindia.com>
2023-07-16 00:02:26 +0530

    deploy

--- feeds/models.py
+++ feeds/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 from django.db import models
-from ckeditor.fields import RichTextField
 
 class PersonalInformation(models.Model):
     name_complete = models.CharField(max_length=50, blank=True, null=True)
